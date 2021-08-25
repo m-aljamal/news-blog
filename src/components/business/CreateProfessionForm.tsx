c425e4e8ea7479d5312c2aaabb554dc1a66b872e
m-aljamal <mohammadjamol@gmail.com>
2021-08-25 23:19:53 +0300

    working on form layout

--- src/components/business/CreateProfessionForm.tsx
+++ src/components/business/CreateProfessionForm.tsx
@@ -9,6 +9,7 @@ import Map from "src/components/business/Map";
 import Head from "next/head";
 import axios from "axios";
 import LoadingSpinner from "../layout/LoadingSpinner";
+import SvgLoading from "../layout/SvgLoading";
 
 interface IProf {
   name: string;
@@ -61,6 +62,189 @@ export default function CreateProfessionForm() {
     setTypeAddress(e.target.value);
   };
 
+  // useEffect(() => {
+  //   const time = setTimeout(async () => {
+  //     try {
+  //       if (typeAddress) {
+  //         const res = await axios.post("/api/profession/getcoordinates", {
+  //           address: typeAddress,
+  //         });
+
+  //         setAutocomplete(res.data);
+  //         setShowAutocomplete(true);
+  //       }
+  //     } catch (error) {
+  //       console.log(error);
+  //       setLoading(false);
+  //     }
+  //   }, 2000);
+  //   return () => clearTimeout(time);
+  // }, [typeAddress]);
+
+  const handleAutoComplete = (place) => {
+    setUserPlace(place.fullAddress);
+    setCoordinates(place.coordinates);
+    setShowAutocomplete(false);
+  };
+
+  // useEffect(() => {
+  //   const listener = (e) => {
+  //     if (!(ref.current! as any).contains(e.target)) {
+  //       setShowAutocomplete(false);
+  //     }
+  //   };
+  //   document.addEventListener("click", listener);
+  //   return () => {
+  //     document.removeEventListener("click", listener);
+  //   };
+  // }, []);
+  return (
+    <>
+      <Head>
+        <link
+          href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <div className="mt-8 container">
+        <h2 className="title mb-8 border-b w-[fit-content] border-red-600 py-2">
+          تسجيل مهنة جديدة:
+        </h2>
+        <SectionContainer title="الاسم ونوع العمل">
+          <WorkAndPersonName register={register} />
+        </SectionContainer>
+
+        <SectionContainer title="شرح عن العمل">
+          <WorkDescription register={register} />
+        </SectionContainer>
+
+        <SectionContainer title="عنوان العمل">
+          <WorkAddress register={register} />
+        </SectionContainer>
+
+        <SectionContainer title="وسائل التواصل">
+          <SocialMedia register={register} />
+        </SectionContainer>
+
+        <div>
+          <h2>صور</h2>
+        </div>
+      </div>
+    </>
+  );
+}
+
+const Input = (props) => {
+  return (
+    <div className={` ${props.style}`}>
+      <p className="text-gray-600">{props.text}</p>
+      {props.type === "textaria" ? (
+        <textarea
+          value={props.value}
+          onChange={props.onChange}
+          {...props.reg}
+          placeholder={props.holder}
+          className="p-2 w-full text-gray-600 text-sm border outline mt-1 bg-gray-100"
+        />
+      ) : (
+        <input
+          id={props.id}
+          {...props.reg}
+          type={props.type}
+          placeholder={props.holder}
+          className={`p-2 w-full text-gray-500 text-sm border outline mt-1 bg-gray-100 ${props.inputStyle}`}
+        />
+      )}
+    </div>
+  );
+};
+
+const List = ({ title, fullAddress, onClick, coordinates }) => {
+  return (
+    <li
+      className="px-4 py-2 cursor-pointer hover:bg-gray-100 "
+      onClick={() => onClick({ fullAddress, coordinates })}
+    >
+      <h2 className="text-gray-800 font-bold text-sm">{title}</h2>
+      <p className="text-gray-400 text-sm">{fullAddress}</p>
+    </li>
+  );
+};
+
+const SectionContainer = ({ children, title }) => {
+  return (
+    <>
+      <div className="flex">
+        <h2 className="title ml-12">{title}</h2>
+        <div className="flex-grow ">{children}</div>
+      </div>
+      <hr className="my-10" />
+    </>
+  );
+};
+
+const WorkAndPersonName = ({ register }) => {
+  return (
+    <div className="grid grid-cols-3 gap-10">
+      <Input text="الاسم والكنية:" reg={register("name")} />
+      <Input text="اسم المحل أو الشركة:" reg={register("businessName")} />
+      <Input
+        text="نوع العمل:"
+        reg={register("businessType")}
+        holder="مثال: تجارة مواد غذائية"
+      />
+    </div>
+  );
+};
+
+const WorkDescription = ({ register }) => {
+  return (
+    <div className="grid grid-cols-2 gap-x-8 gap-y-4 ">
+      <Input
+        text="شرح عن الخدمات المقدمة:"
+        reg={register("jobDescription")}
+        holder="مثال: مبيع الخضراوات والفواكه بجميع انواعها"
+        type="textaria"
+      />
+      <Input
+        text="لمحة عن المحل او الشركة:"
+        reg={register("description")}
+        holder="مثال: تأسست الشركة عام 2015 وهي الان احد اقوى الشركات التجارية"
+        type="textaria"
+      />
+      <Input
+        text="سنوات الخبرة في العمل:"
+        reg={register("businessStart")}
+        holder="مثال: 5 سنوات"
+      />
+      <Input
+        text="عدد الموظفين:"
+        reg={register("NumberOfEmployees")}
+        type="number"
+      />
+    </div>
+  );
+};
+
+const WorkAddress = ({ register }) => {
+  const [userPlace, setUserPlace] = useState("");
+  const [coordinates, setCoordinates] = useState([35.2433, 38.9637]);
+  const [typeAddress, setTypeAddress] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [autocomplete, setAutocomplete] = useState(null);
+  const [showAutocomplete, setShowAutocomplete] = useState(false);
+  const ref = useRef();
+  const handleCoordinates = async (e) => {
+    setLoading(true);
+    setUserPlace(e.target.value);
+    setTypeAddress(e.target.value);
+  };
+  const handleAutoComplete = (place) => {
+    setUserPlace(place.fullAddress);
+    setCoordinates(place.coordinates);
+    setShowAutocomplete(false);
+  };
+
   useEffect(() => {
     const time = setTimeout(async () => {
       try {
@@ -71,7 +255,6 @@ export default function CreateProfessionForm() {
 
           setAutocomplete(res.data);
           setShowAutocomplete(true);
-          // setCoordinates(res.data.coordinates);
         }
       } catch (error) {
         console.log(error);
@@ -80,12 +263,6 @@ export default function CreateProfessionForm() {
     }, 2000);
     return () => clearTimeout(time);
   }, [typeAddress]);
-
-  const handleAutoComplete = (place) => {
-    setUserPlace(place.fullAddress);
-    setCoordinates(place.coordinates);
-    setShowAutocomplete(false);
-  };
 
   useEffect(() => {
     const listener = (e) => {
@@ -99,179 +276,108 @@ export default function CreateProfessionForm() {
     };
   }, []);
   return (
-    <>
-      <Head>
-        <link
-          href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
-          rel="stylesheet"
-        />
-      </Head>
-      <div className=" mt-8 ">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="container">
-            <h2 className="title mb-4">تسجيل مهنة جديدة:</h2>
-            <div className="sm:flex sm:gap-8  ">
-              <div className="sm:w-1/2">
-                <Input text="الاسم والكنية:" reg={register("name")} />
-                <Input
-                  text="اسم المحل أو الشركة:"
-                  reg={register("businessName")}
-                />
-                <Input
-                  text="نوع العمل:"
-                  reg={register("businessType")}
-                  holder="مثال: تجارة مواد غذائية"
-                />
-                <Input
-                  text="شرح عن الخدمات المقدمة:"
-                  reg={register("jobDescription")}
-                  holder="مثال: مبيع الخضراوات والفواكه بجميع انواعها"
-                  type="textaria"
-                />
-                <Input
-                  text="لمحة عن المحل او الشركة:"
-                  reg={register("description")}
-                  holder="مثال: تأسست الشركة عام 2015 وهي الان احد اقوى الشركات التجارية"
-                  type="textaria"
-                />
-                <Input
-                  text="سنوات الخبرة في العمل:"
-                  reg={register("businessStart")}
-                  holder="مثال: 5 سنوات"
-                />
-                <Input
-                  text="عدد الموظفين:"
-                  reg={register("NumberOfEmployees")}
-                  type="number"
-                />
-              </div>
-              <div className="sm:w-1/2">
-                <div className="">
-                  <Input
-                    text="العنوان:"
-                    reg={register("country")}
-                    holder="الدولة"
-                  />
-                  <div className="relative" ref={ref}>
-                    <Input
-                      holder="الافضل كتابة العنوان حسب لغة الدولة"
-                      type="textaria"
-                      onChange={handleCoordinates}
-                      style="my-0"
-                      value={userPlace}
-                    />
-                    <ul className="absolute   z-50 shadow-md bg-white w-full rounded-md">
-                      {/* <List title="kilis" fullAddress="Kilis, Turkey" />
-                      <List title="gaz" fullAddress="Kilis, Hastani yol" /> */}
-                      {showAutocomplete &&
-                        autocomplete.map((l) => (
-                          <List
-                            title={l.text}
-                            fullAddress={l.place_name}
-                            key={l.id}
-                            onClick={handleAutoComplete}
-                            coordinates={l.geometry.coordinates}
-                          />
-                        ))}
-                    </ul>
-                  </div>
-                  <div className="rounded-lg shadow-md">
-                    <Map title={"address"} coordinates={coordinates} />
-                  </div>
-                </div>
-                <div>
-                  <Input
-                    text="وسائل التواصل:"
-                    reg={register("phone")}
-                    holder="رقم الهاتف مثال: 009053975914266"
-                  />
-                  <Input
-                    reg={register("whatsAppNumber")}
-                    holder="رقم وتس اب مثال: 009053975914266"
-                  />
-                  <Input
-                    reg={register("faceBook")}
-                    holder="رابط صفحة فيس بوك"
-                  />
-                  <Input
-                    reg={register("instagram")}
-                    holder="رابط صفحة انستغرام"
-                  />
-                  <Input reg={register("youtube")} holder="رابط قناة يوتيوب" />
-                  <Input
-                    reg={register("website")}
-                    holder="رابط الموقع الشخصي"
-                  />
-                  <Input reg={register("email")} holder="الايميل الشخصي" />
-                </div>
-
-                {/* <div>
-                <p className="title">اختيار اللوغو:</p>
-                <ChoseImage
-                  error={null}
-                  previewImage={chosenLogo}
-                  setPreviewImage={setChosenLogo}
-                />
-              </div> */}
-              </div>
-            </div>
+    <div className="grid grid-cols-2 gap-x-8 gap-y-4 ">
+      <Input
+        type="textaria"
+        text="الدولة:"
+        reg={register("country")}
+        holder="مثال: تركيا"
+      />
+      <div>
+        <div className="relative" ref={ref}>
+          <div className="relative ">
+            <SvgLoading />
+            <Input
+              text="العنوان:"
+              holder="الافضل كتابة العنوان حسب لغة الدولة"
+              type="textaria"
+              onChange={handleCoordinates}
+              value={userPlace}
+            />
           </div>
-          <div className="bg-white border sticky bottom-0 py-2 z-20">
-            <div className="flex justify-around text-white">
-              <button
-                type="submit"
-                className="border px-3 py-1 bg-blue rounded-md w-32"
-              >
-                تسجيل
-              </button>
-              <Link href="/profession">
-                <button
-                  type="button"
-                  className="border px-3 py-1 bg-red-500 rounded-md w-32"
-                >
-                  الغاء
-                </button>
-              </Link>
-            </div>
-          </div>
-        </form>
+          <ul className="absolute   z-50 shadow-md bg-white w-full rounded-md">
+            {showAutocomplete &&
+              autocomplete.map((l) => (
+                <List
+                  title={l.text}
+                  fullAddress={l.place_name}
+                  key={l.id}
+                  onClick={handleAutoComplete}
+                  coordinates={l.geometry.coordinates}
+                />
+              ))}
+          </ul>
+        </div>
       </div>
-    </>
-  );
-}
-
-const Input = (props) => {
-  return (
-    <div className={`my-4 ${props.style}`}>
-      <p className="title">{props.text}</p>
-      {props.type === "textaria" ? (
-        <textarea
-          value={props.value}
-          onChange={props.onChange}
-          {...props.reg}
-          placeholder={props.holder}
-          className="p-2 w-full text-gray-500 text-sm border outline mt-1"
-        />
-      ) : (
-        <input
-          {...props.reg}
-          type={props.type}
-          placeholder={props.holder}
-          className="p-2 w-full text-gray-500 text-sm border outline mt-1 bg-gray-100"
-        />
-      )}
+      <div className="rounded-lg shadow-md col-start-1 col-end-3">
+        <Map title={"address"} coordinates={coordinates} />
+      </div>
     </div>
   );
 };
 
-const List = ({ title, fullAddress, onClick, coordinates }) => {
+const SocialMedia = ({ register }) => {
   return (
-    <li
-      className="px-4 py-2 cursor-pointer hover:bg-gray-100 "
-      onClick={() => onClick({ fullAddress, coordinates })}
-    >
-      <h2 className="text-gray-800 font-bold">{title}</h2>
-      <p className="text-gray-400 ">{fullAddress}</p>
-    </li>
+    <div>
+      <div className="grid grid-cols-3 gap-x-8 gap-y-4 ">
+        <InputWithIcon icon="fas fa-phone text-blue">
+          <Input
+            reg={register("phone")}
+            holder="رقم الهاتف مثال: 009053975914266"
+            inputStyle="pr-9"
+          />
+        </InputWithIcon>
+        <InputWithIcon icon="fab fa-whatsapp text-green-600">
+          <Input
+            reg={register("whatsAppNumber")}
+            holder="رقم وتس اب مثال: 009053975914266"
+            inputStyle="pr-9"
+          />
+        </InputWithIcon>
+        <InputWithIcon icon="fab fa-facebook text-blue">
+          <Input
+            id="faceBook"
+            reg={register("faceBook")}
+            holder="رابط صفحة فيس بوك"
+            inputStyle="pr-9"
+          />
+        </InputWithIcon>
+        <InputWithIcon icon="fab fa-instagram text-pink-500">
+          <Input
+            reg={register("instagram")}
+            holder="رابط صفحة انستغرام"
+            inputStyle="pr-9"
+          />
+        </InputWithIcon>
+        <InputWithIcon icon="fab fa-youtube text-red-500 ">
+          <Input
+            reg={register("youtube")}
+            holder="رابط قناة يوتيوب"
+            inputStyle="pr-9"
+          />
+        </InputWithIcon>
+        <InputWithIcon icon="fas fa-globe text-green-500">
+          <Input
+            reg={register("website")}
+            holder="رابط الموقع الالكتروني"
+            inputStyle="pr-9"
+          />
+        </InputWithIcon>
+        <InputWithIcon icon="fas fa-envelope text-red-500 ">
+          <Input reg={register("email")} holder="الايميل" inputStyle="pr-9" />
+        </InputWithIcon>
+      </div>
+    </div>
+  );
+};
+
+const InputWithIcon = ({ children, icon }) => {
+  return (
+    <label htmlFor="faceBook" className="relative">
+      <i
+        className={` ${icon}  fa-lg absolute top-1/2 transform -translate-y-1/2 right-3`}
+      ></i>
+      {children}
+    </label>
   );
 };
